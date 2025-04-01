7b53c54035422f3a140437b1069a03ead96fda9b
Richie Zhang <richiezhang@protonmail.com>
2025-03-31 17:43:57 -0700

    Create LED test script

--- led.py
+++ led.py
@@ -3,6 +3,16 @@
 import RPi.GPIO as GPIO
 import time
 
+# The GPIO pins for each colour LED
+RED = 2
+YELLOW = 3
+BLUE = 4
+GREEN = 17
+WHITE = 27
+
+# The order of LEDS from representing highest to lowest frequencies
+LEDS = [RED, YELLOW, BLUE, GREEN, WHITE]
+
 def gpio_setup():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
@@ -11,8 +21,17 @@ def set_output(pin: int):
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, False)
 
+def led_setup(leds: list[int]):
+    gpio_setup()
+    for led in leds:
+        set_output(led)
+
+def led_clear(leds: list[int]):
+    for led in leds:
+        GPIO.output(led, False)
+
 def blink(pin: int, interval: int):
     GPIO.output(pin, True)
-    time.sleep(interval)
+    time.sleep(1000)
     GPIO.output(pin, False)
 
---
+++ led_test.py
@@ -0,0 +1,13 @@
+from led import *
+
+def main():
+    led_setup(LEDS)
+    while True:
+        for led in LEDS:
+            blink(led, 1)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        led_clear(LEDS)
